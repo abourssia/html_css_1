2541f6f6bb34c5a18dc6d2f855afd0b90aae2761
winerrordoth <abourssia@gmail.com>
2022-12-24 14:59:44 +0100

    Some things don't change

---
+++ index.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <!-- Font Awesome -->
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+    />
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.0/animate.min.css">
+    <!-- Bootstrap core CSS -->
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.3.1/css/hover.css" integrity="sha256-JN6PzDiVqV2siZjedqNB10DTVZsdRKY5oW0TTvYpAuQ=" crossorigin="anonymous" />
+    <!-- Material Design Bootstrap -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="css/style.css" />
+
+    <title> LOREM </title>
+  </head>
+  <body>
+    <header class="header">
+      <nav class="navbar navbar-expand-lg navbar-light ">
+        <a class="navbar-brand" href="#"></a>
+        <div class="logo-box">
+          <a href="index.php">
+            <img src="img/logo.png" class="logo" />
+          </a>
+        </div>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#Home"> Home</a>
+            </li>
+            <li class="nav-item">
+
+                    <a class="nav-link" href ="" data-toggle="modal" data-target="#Explore">
+                            Explore</a></li>
+                            <li class="nav-item">
+              <a class="nav-link" href ="" data-toggle="modal" data-target="#ContactUs"> Contact Us</a>
+            </li>
+           <li class="nav-item">
+
+                    <a class="nav-link" href ="" data-toggle="modal" data-target="#Search">
+                    <i style="font-size:16px;" class="fas fa-search"></i>
+                    </a>            </li>
+            
+          
+          </ul>
+        </div>
+      </nav>
+      </header>
+      <div class="text-box">
+        <h1 class="primary-heading">
+          <span class="heading-main">
+           I P S <strong class="stg">U</strong> M
+          </span>
+          <span class="heading-sub">
+       
+               Con<strong class="st">s</strong>e<strong class="st">c</strong>teteur<strong class="st">
+              n</strong>s <strong class="st">S</strong><strong class="st">
+                c</strong>adipisicing<strong class="st">e</strong>lit
+          </span>
+        </h1>
+
+        <a href="#" class="btnn"> SOMETHING </a>
+      </div>
+   
+    <!--- Explore MODAL -->
+    <div class="modal" id="Explore">
+            <div class="modal-dialog modal-Explore">
+              <div class="modal-content">
+          
+                <!-- Modal Header -->
+                <div class="modal-header" style="border: none;">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+          
+                <!-- Modal body -->
+                <div class="modal-body">
+                        Explore body..
+                </div>
+          
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+          
+              </div>
+            </div>
+          </div>
+
+    <!--- SEARCH MODAL -->
+  <div class="modal" id="Search">
+        <div class="modal-dialog modal-full modal-search">
+          <div class="modal-content">
+      
+            <!-- Modal Header -->
+            <div class="modal-header" style="border: none;">
+              <!-- --><button type="button" class="close" data-dismiss="modal">&times;</button>
+
+              
+            </div>
+      
+            <!-- Modal body -->
+            <div class="modal-body">
+              Search body..
+            </div>
+      
+            <!-- Modal footer -->
+          <!-- <div class="modal-footer">
+              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div> -->  
+      
+          </div>
+        </div>
+      </div>
+
+
+      
+    <!--- ContactUs MODAL -->
+  <div class="modal" id="ContactUs">
+        <div class="modal-dialog modal-full modal-ContactUs">
+          <div class="modal-content">
+      
+            <!-- Modal Header -->
+            <div class="modal-header" style="border: none;">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+      
+            <!-- Modal body -->
+            <div class="modal-body">
+                    ContactUs body..
+            </div>
+      
+            <!-- Modal footer -->
+            <div class="modal-footer">
+              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+      
+          </div>
+        </div>
+      </div>
+      
+      <div class="sidenav ">
+      <p>       <i class="fa fa-arrow-left bars" aria-hidden="true"></i>
+Something </p>
+</div>
+
+
+<!--  something model-->
+
+
+
+      <!--  something model-->
+
+    <!-- Footer -->
+    <footer>
+      <div class="footer">
+        <div class="coright">
+          <p>
+              <ul>
+                  <li> Get connected with us on social networks!
+                    </li>
+                  <li><a href=""><i class="fab fa-facebook-f"></i> </a>
+                  </li>
+                  <li><a href=""><i class="fab fa-twitter"></i></a>
+                  </li>                  
+                  <li><a href=""><i class="fab fa-linkedin-in"></i></a>
+                  </li>
+                  <li><a href=""><i class="fas fa-envelope"></i></a>
+                  </li>
+              </ul>
+          </p>
+        </div>
+        <div class="row">
+              <div class="statistics">
+          <div class="item">
+            <i class="fas fa-users"></i>
+            <div class="item-desc">
+              <h1  class="count">16</h1>
+              <p>Users</p>
+            </div>
+          </div>
+          <div class="item">
+            <i class="fas fa-clock"></i>
+            <div class="item-desc">
+              <h1 class="count">35</h1>
+              <p>Events</p>
+            </div>
+          </div>
+          <div class="item">
+            <i class="fas fa-calendar-alt"></i>
+            <div class="item-desc">
+              <h1 class="count">15</h1>
+              <p>Upcoming Events</p>
+            </div>
+          </div>
+        </div>
+        </div></div>
+      <div class="footer-copyright">
+            © 2019 Copyright: ATRST.DZ
+      </div>
+    </footer>
+    <!-- Footer -->
+
+    <!-- JQuery -->
+    <script
+      type="text/javascript"
+      src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"
+    ></script>
+    <!-- Bootstrap tooltips -->
+    <script
+      type="text/javascript"
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"
+    ></script>
+    <!-- Bootstrap core JavaScript -->
+    <script
+      type="text/javascript"
+      src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"
+    ></script>
+    <!-- MDB core JavaScript -->
+    <script
+      type="text/javascript"
+      src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.3/js/mdb.min.js"
+    ></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="js/animatedModal.min.js"></script>
+<script>
+
+
+
+function testAnim(x) {
+    $('.modal .modal-dialog').attr('class', 'modal-dialog  ' + x + '  animated');
+};
+$('#Search').on('show.bs.modal', function (e) {
+  var anim = $('#entrance').val();
+      testAnim(anim);
+})
+$('#Search').on('hide.bs.modal', function (e) {
+  var anim = $('#exit').val();
+      testAnim(anim);
+})
+
+
+</script>
+
+
+<script>
+
+
+$(window).resize(function() {
+  if ($(window).width() <= 768) {
+    $('.sidenav').remove().insertAfter($('.header'));
+  } else {
+    $('.sidenav').remove().insertBefore($('.header'));
+  }
+})
+</script>
+<script>
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+</script>
+  </body>
+
+
+
+</html>
